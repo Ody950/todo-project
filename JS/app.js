928d6743e49753cb8c45fd81d94403e111984cb5
Ody950 <odai.al.obeidat950@gmail.com>
2023-04-05 09:00:08 +0000

    06

--- JS/app.js
+++ JS/app.js
@@ -39,85 +39,47 @@ if (confirm("Do you want skip the welcoming message?")) {
 
 
 
-
-
-
-let Array1 = [];
+  function QuestionsAndArray(question1, question2, question3) {
+    const answers = [];
   
-
-function questionOneFun () {
-  let question_1 = prompt("Does the time matter to you?(yes/no)");
-  
-  for (let i = 0; i < 1; i++) {
     
-    if (question_1 === "") {
-      Array1.push("Question 1 answer is invalid");
-    
-    } else if (question_1.toLowerCase() === "yes") {
-      Array1.push("Question 1 answer: " + question_1);
-    
-    } else if (question_1.toLowerCase() === "no") {
-      Array1.push("Question 1 answer: " + question_1);
-    } else {
-      question_1 = prompt("Does the time matter to you?(yes/no)");
+    let answer1 = prompt("Does the time matter to you?(yes/no)" + question1);
+    while (answer1.toLowerCase() !== "yes" && answer1.toLowerCase() !== "no") {
+      if (answer1 === "" || answer1 === null) {
+        answer1 = "invalid";
+        break;
+      } else {
+        answer1 = prompt("Does the time matter to you?(yes/no)" + question1);
+      }
     }
-    
-  }
+    answers.push(answer1);
   
-  }
-
-questionOneFun();
-
-
-function questionTwoFun () {
-  let question_2 = prompt("Are you confident that this website will allow you to organize your tasks in order to make the best use of your time (yes/no)");
-  
-  for (let i = 0; i < 1; i++) {
     
-    if (question_2 === "") {
-      Array1.push("Question 2 answer is invalid");
-    
-    } else if (question_2.toLowerCase() === "yes") {
-      Array1.push("Question 2 answer: " + question_2);
-    
-    } else if (question_2.toLowerCase() === "no") {
-      Array1.push("Question 2 answer: " + question_2);
-    } else {
-      question_2 = prompt("Does the time matter to you?(yes/no)");
+    let answer2 = prompt("Are you confident that this website will allow you to organize your tasks in order to make the best use of your time (yes/no)" + question2);
+    while (answer2.toLowerCase() !== "yes" && answer2.toLowerCase() !== "no") {
+      if (answer2 === "" || answer2 === null) {
+        answer2 = "invalid";
+        break;
+      } else {
+        answer2 = prompt("Are you confident that this website will allow you to organize your tasks in order to make the best use of your time (yes/no)" + question2);
+      }
     }
-    
-  }
+    answers.push(answer2);
   
-  }
-
-questionTwoFun();
-
-
-function questionThreeFun () {
-  let question_3 = prompt("Are you of the opinion that the only time to rest should be when you sleep(yes/no)");
-
-  for (let i = 0; i < 1; i++) {
     
-    if (question_3 === "") {
-      Array1.push("Question 3 answer is invalid");
-    
-    } else if (question_3.toLowerCase() === "yes") {
-      Array1.push("Question 3 answer: " + question_3);
-    
-    } else if (question_3.toLowerCase() === "no") {
-      Array1.push("Question 3 answer: " + question_3);
-    } else {
-      question_3 = prompt("Does the time matter to you?(yes/no)");
+    let answer3 = prompt("Are you of the opinion that the only time to rest should be when you sleep(yes/no)" + question3);
+    while (answer3.toLowerCase() !== "yes" && answer3.toLowerCase() !== "no") {
+      if (answer3 === "" || answer3 === null) {
+        answer3 = "invalid";
+        break;
+      } else {
+        answer3 = prompt("Are you of the opinion that the only time to rest should be when you sleep(yes/no)" + question3);
+      }
     }
+    answers.push(answer3);
+    for (let i = 0; i < answers.length; i++) {
+      console.log(answers[i]);}
+      return answers;
     
   }
-  
-  }
-
-questionThreeFun();
-
-
-for (let i = 0; i < Array1.length; i++) {
-  console.log(Array1[i]);
-}
-
+  QuestionsAndArray()
